97eb61be704bd8a8d501f2f65e8bd2018305699f
Brian Feister <brian@drm.st>
2012-09-09 05:23:35 -0400

    Add foutbgone and doc.ready

--- wp-content/themes/casbah_dance_custom/assets/js/main.js
+++ wp-content/themes/casbah_dance_custom/assets/js/main.js
@@ -1,7 +1,18 @@
-/* Author:
+/*
+
+Author: Brian Feister (Dreamstar Studios)
 
 */
 
+fbg.hideFOUT('asap', 400);
+
+// start document.ready
+( function($) { $(document).ready(function () {
+
+	// do stuff
+
+
+}); } ) ( jQuery ); // end document.ready
 
 
 
